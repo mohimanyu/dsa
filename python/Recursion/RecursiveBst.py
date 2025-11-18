9c2dff031f5a9ff111c795dbc70ee1fc252b593d
Mohimanyu Bharti <mohimanyu.bharti@neudesic.com>
2025-11-19 05:03:55 +0530

    Refactor RecursiveBST class: rename methods for clarity and improve node deletion logic

--- python/Recursion/RecursiveBst.py
+++ python/Recursion/RecursiveBst.py
@@ -5,13 +5,13 @@ class Node:
         self.right = None
 
 
-class BinarySearchTree:
+class RecursiveBST:
     def __init__(self):
         self.root = None
 
     def __r_insert(self, current_node, value):
         if current_node == None:
-            return Node(value)
+            current_node = Node(value)
         if value < current_node.value:
             current_node.left = self.__r_insert(current_node.left, value)
         if value > current_node.value:
@@ -19,14 +19,14 @@ class BinarySearchTree:
         return current_node
 
     def r_insert(self, value):
-        if self.root == None:
+        if not self.root:
             self.root = Node(value)
         return self.__r_insert(self.root, value)
 
     def __r_contains(self, current_node, value):
-        if current_node == None:
+        if not current_node:
             return False
-        if value == current_node.value:
+        if current_node.value == value:
             return True
         if value < current_node.value:
             return self.__r_contains(current_node.left, value)
@@ -39,54 +39,44 @@ class BinarySearchTree:
     def min_value(self, current_node):
         while current_node.left:
             current_node = current_node.left
+
         return current_node.value
 
-    def __delete_node(self, current_node, value):
-        if current_node == None:
+    def __r_delete(self, current_node, value):
+        if not current_node:
             return None
         if value < current_node.value:
-            current_node.left = self.__delete_node(current_node.left, value)
-        elif value > current_node.value:
-            current_node.right = self.__delete_node(current_node.right, value)
+            current_node.left = self.__r_delete(current_node.left, value)
+        elif value < current_node.value:
+            current_node.right = self.__r_delete(current_node.right, value)
         else:
-            if current_node.left == None and current_node.right == None:
-                return None
-            elif current_node.left == None:
+            if not current_node.left and not current_node.right:
+                current_node = None
+            elif not current_node.left:
                 current_node = current_node.right
-            elif current_node.right == None:
+            elif not current_node.right:
                 current_node = current_node.left
             else:
                 sub_tree_min = self.min_value(current_node.right)
                 current_node.value = sub_tree_min
-                current_node.right = self.__delete_node(
-                    current_node.right, sub_tree_min
-                )
+                current_node.right = self.__r_delete(current_node.right, sub_tree_min)
+
         return current_node
 
-    def delete_node(self, value):
-        self.__delete_node(self.root, value)
+    def r_delete(self, value):
+        return self.__r_delete(self.root, value)
 
 
-# nums = [47, 21, 76, 18, 27, 52, 82]
-
-my_tree = BinarySearchTree()
+my_tree = RecursiveBST()
 my_tree.r_insert(2)
 my_tree.r_insert(1)
 my_tree.r_insert(3)
 
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
-
-my_tree.delete_node(2)
+my_tree.r_delete(2)
 
 print(my_tree.root.value)
 print(my_tree.root.left.value)
 print(my_tree.root.right)
 
-# for num in nums:
-#     my_tree.r_insert(num)
-
-# print(my_tree.r_contains(27))
-
-# print(my_tree.r_contains(17))
+print(my_tree.r_contains(2))
+print(my_tree.min_value(my_tree.root))
